f71e16bd14eecf9cdd68412d9afa409fdb98cd00
klosjustyna <klos.justyna@wp.pl>
2020-03-26 09:30:21 +0100

    nwd

---
+++ dockerlab200326/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const redis = require('redis');
+
+const app = express();
+
+const process = require('process');
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
+
+//  client.set('counter', 0);
+ client.set('record', 0);
+
+app.get('/numberOne/numberTwo', (req, resp) => {
+
+    console.log('New action');
+    process.exit(0);
+    while(numberOne != numberTwo){
+        if(numberOne > numberTwo)
+        numberOne = numberOne - numberTwo;
+        else 
+        numberTwo = numberTwo - numberOne;
+    }  
+        client.set('numberOne', parseInt(numberOne));
+    
+    // client.get('numberOne', (err, numberOne) => {
+    //     resp.send('numberOne: ' / number);
+    //     client.set('numberOne', parseInt(numberOne) + 1);
+    // }); }
+});
+
+app.listen(8080, () => {
+    console.log('Server listening on port 8001');
+});
